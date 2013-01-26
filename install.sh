6a859c2525a676d871aeba8ded18c317139a910b
Jesse Gumm <sigmastar@gmail.com>
2013-01-26 17:56:24 -0600

    add vim seek

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ cd bundle
 git clone git://github.com/choptastic/vimerl.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone git://github.com/jceb/vim-orgmode.git
+git clone git://github.com/goldfeld/vim-seek.git
 
 cd ..
 
